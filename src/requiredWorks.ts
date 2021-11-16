905a9e3d081d60a3eaadaea86b9b0dd4cdd5aad2
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2021-11-16 00:22:52 -0300

    Added a bunch of things

--- src/requiredWorks.ts
+++ src/requiredWorks.ts
@@ -2,66 +2,81 @@ export const trabalhosObrigatorios: Work[] = [
   {
     name: 'Pesquisa sobre as funções orgânicas',
     area: 'Natureza',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1q4nCNAfAWJuiGAn6XwfKtscIyhLO75hi1aTmNhWY3wQ/edit?usp=sharing',
   },
   {
     name: 'Desafio Trimestral',
     area: 'Natureza',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1IlSXLuAf7MdgFvSckvudurNYxXCeaOOH_K_OnOfx0Uc/edit?usp=sharing',
   },
   {
     name: 'Eletroimã',
     area: 'Natureza',
-    url: '',
+    url: '#',
   },
   {
     name: 'Podcast',
     area: 'Natureza',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1EDHCV-kH3hUMr8pmKCACpAzvOpLqbfAo/view?usp=sharing',
   },
   {
     name: 'Exposição fotográfica industrialização',
     area: 'Humanas',
-    url: '',
+    url: 'https://galeria-humanas.herokuapp.com',
   },
   {
     name: 'Pesquisa dos países',
     area: 'Humanas',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1sS8ao0qi2uaxct0jof-FwHYVQ3rB6vxgAZ3XVmgELBI/edit?usp=sharing',
   },
   {
     name: 'Pesquisa da hemeroteca digital',
     area: 'Humanas',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/18ATWgNQzFD4Dgm13FQrDcEaz3CrWp5_pxCFu4iG-FYo/edit?usp=sharing',
   },
   {
     name: 'Redação do ENEM',
     area: 'Linguagens',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1MMhcS9NF5S0f9yy_0oJalRa1P8_C7ijN/view?usp=sharing',
   },
   {
     name: 'Obra da Semana da Arte',
     area: 'Linguagens',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1P6akNOV1NpFxjo4h7pPglc6lyM6AD7z3/view?usp=sharing',
+  },
+  {
+    name: 'Releitura de Drummond',
+    area: 'Linguagens',
+    url:
+      'https://drive.google.com/file/d/1QuBMawNezoLLjMDobuhUqaytuTCLF01V/view?usp=sharing',
   },
   {
     name: 'Mapa mental',
     area: 'Matemática',
-    url: '',
+    url: 'https://www.mindmeister.com/beta/1828942885?t=KRBdnghoZM',
   },
   {
     name: 'Atividade Probabilidade',
     area: 'Matemática',
-    url: '',
+    url:
+      'https://docs.google.com/spreadsheets/d/1Q3B1h9XOM-CpUAQCG0AVpj0omgX50cCOyGWnQblKm9Q/edit?usp=sharing',
   },
   {
     name: 'Pesquisa aplicações Número Complexos',
     area: 'Matemática',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1nCEA35jOnvcpKrbyAssDaMCLtdRZhvPr/view?usp=sharing',
   },
   {
     name: 'Semana da Arte',
     area: 'Técnico',
-    url: '',
+    url: 'https://mosqueteiros-semana.herokuapp.com/',
   },
 ]
--- src/works.ts
+++ src/works.ts
@@ -2,155 +2,182 @@ export const trabalhosNotaveis: Work[] = [
   {
     name: 'Ficha de Leitura',
     area: 'Linguagens',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1v4-hsEoVoSu3z-iUHMXrRQmhSndgC33_eIjDPFcoc70/edit?usp=sharing',
   },
   {
     name: 'Poesia "Gloriosa"',
     area: 'Linguagens',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1gj0Sz6jj4AxRus1jur7jaRhYIq3rIjJy/view?usp=sharing',
   },
   {
     name: 'Pesquisa sobre Behr',
     area: 'Linguagens',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1LhHQx8grs-jNwtMi1kGyKhsQrFyinz63qNcSMJ4z5as/edit?usp=sharing',
   },
   {
     name: 'Projeto Semana da Arte',
     area: 'Linguagens',
-    url: '',
+    url: 'https://mosqueteiros-semana.herokuapp.com/',
   },
   {
     name: 'Pesquisa sobre o Nazismo',
     area: 'Humanas',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1ghoX6K7GAdLdaqLGyjwhY1RdQK_B5LTe/view?usp=sharing',
   },
   {
     name: 'Pesquisa sobre a Mongólia',
     area: 'Humanas',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/14WBqKfb5j58RVI0fmvKDnbzLGPtXYlZ01N_jq875SBg/edit?usp=sharing',
   },
   {
     name: `Pesquisa do jornal L'Italiano`,
     area: 'Humanas',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1go-cOfJcJARMCHY4RNPVus4mXvIBdPW8Cgky45tCVDM/edit?usp=sharing',
   },
   {
-    name: 'Gráficos sobre o Brasil',
-    area: 'Natureza',
-    url: '',
+    name: 'Modelo em Cascasta',
+    area: 'Técnico',
+    url:
+      'https://docs.google.com/presentation/d/1IuwrdwKTjR6Vgm2mTiajm9spZhdmca0Vg5Y6fqOqWXU/edit?usp=sharing',
   },
   {
     name: 'Pesquisa sobre as funções orgânicas',
     area: 'Natureza',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1bCfsm1MsidhrtSnCr7t0vgsef3Fyc2rXq1xm7e5fT64/edit?usp=sharing',
   },
   {
     name: 'Juri Simulado',
     area: 'Natureza',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1xwuyVv-WRDize-45ZrXALaBq9ICHjvnLn4rfz5oBHlA/edit?usp=sharing',
   },
 ]
 export const notableWorks: Work[] = [
   {
     name: 'Reading Report',
     area: 'Language',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1v4-hsEoVoSu3z-iUHMXrRQmhSndgC33_eIjDPFcoc70/edit?usp=sharing',
   },
   {
     name: 'Poem "Gloriosa"',
     area: 'Language',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1gj0Sz6jj4AxRus1jur7jaRhYIq3rIjJy/view?usp=sharing',
   },
   {
     name: 'Research about Behr',
     area: 'Language',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1LhHQx8grs-jNwtMi1kGyKhsQrFyinz63qNcSMJ4z5as/edit?usp=sharing',
   },
   {
     name: 'Project Semana da Arte',
     area: 'Languages',
-    url: '',
+    url: 'https://mosqueteiros-semana.herokuapp.com/',
   },
   {
     name: 'Research about Nazism',
     area: 'Humane',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1ghoX6K7GAdLdaqLGyjwhY1RdQK_B5LTe/view?usp=sharing',
   },
   {
     name: 'Research about Mongolia',
     area: 'Humane',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/14WBqKfb5j58RVI0fmvKDnbzLGPtXYlZ01N_jq875SBg/edit?usp=sharing',
   },
   {
     name: `Research on the periodic L'Italiano`,
     area: 'Humane',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1go-cOfJcJARMCHY4RNPVus4mXvIBdPW8Cgky45tCVDM/edit?usp=sharing',
   },
   {
-    name: 'Graphics about Brazil',
-    area: 'Natural',
-    url: '',
+    name: 'Waterfall Methodology',
+    area: 'Technic',
+    url:
+      'https://docs.google.com/presentation/d/1IuwrdwKTjR6Vgm2mTiajm9spZhdmca0Vg5Y6fqOqWXU/edit?usp=sharing',
   },
   {
     name: 'Research about organic functional groups',
     area: 'Natural',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1bCfsm1MsidhrtSnCr7t0vgsef3Fyc2rXq1xm7e5fT64/edit?usp=sharing',
   },
   {
     name: 'Simulated Jury',
     area: 'Natural',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1xwuyVv-WRDize-45ZrXALaBq9ICHjvnLn4rfz5oBHlA/edit?usp=sharing',
   },
 ]
 export const CompitiNotevole: Work[] = [
   {
     name: 'Rapporto di Lettura',
     area: 'Linguaggio',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1v4-hsEoVoSu3z-iUHMXrRQmhSndgC33_eIjDPFcoc70/edit?usp=sharing',
   },
   {
     name: 'Poesia "Gloriosa"',
     area: 'Linguaggio',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1gj0Sz6jj4AxRus1jur7jaRhYIq3rIjJy/view?usp=sharing',
   },
   {
     name: 'Ricerca su Behr',
     area: 'Linguaggio',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/1LhHQx8grs-jNwtMi1kGyKhsQrFyinz63qNcSMJ4z5as/edit?usp=sharing',
   },
   {
     name: 'Progetto Semana da Arte',
     area: 'Linguaggio',
-    url: '',
+    url: 'https://mosqueteiros-semana.herokuapp.com/',
   },
   {
     name: 'Ricerca sul Nazismo',
     area: 'Umanistiche',
-    url: '',
+    url:
+      'https://drive.google.com/file/d/1ghoX6K7GAdLdaqLGyjwhY1RdQK_B5LTe/view?usp=sharing',
   },
   {
     name: 'Ricerca sulla Mongolia',
     area: 'Umanistiche',
-    url: '',
+    url:
+      'https://docs.google.com/presentation/d/14WBqKfb5j58RVI0fmvKDnbzLGPtXYlZ01N_jq875SBg/edit?usp=sharing',
   },
   {
     name: `Ricerca sul periodico L'Italiano`,
     area: 'Umanistiche',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1go-cOfJcJARMCHY4RNPVus4mXvIBdPW8Cgky45tCVDM/edit?usp=sharing',
   },
   {
-    name: 'Grafici sul Brasil',
-    area: 'Naturale',
-    url: '',
+    name: 'Metodologia Cascata',
+    area: 'Tecnico',
+    url:
+      'https://docs.google.com/presentation/d/1IuwrdwKTjR6Vgm2mTiajm9spZhdmca0Vg5Y6fqOqWXU/edit?usp=sharing',
   },
   {
     name: 'Ricerca suglle funzioni organiche',
     area: 'Naturale',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1bCfsm1MsidhrtSnCr7t0vgsef3Fyc2rXq1xm7e5fT64/edit?usp=sharing',
   },
   {
     name: 'Juri Simulato',
     area: 'Naturale',
-    url: '',
+    url:
+      'https://docs.google.com/document/d/1xwuyVv-WRDize-45ZrXALaBq9ICHjvnLn4rfz5oBHlA/edit?usp=sharing',
   },
 ]
