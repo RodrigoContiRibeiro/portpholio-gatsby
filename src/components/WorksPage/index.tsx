bbf867cf8edd8ce7177ff583c19bf3a2663cf495
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2021-11-29 22:17:25 -0300

    Removed a window declaration from the return of a react component,instead turned it to a state and setted it through the life-cycle

--- src/components/WorksPage/index.tsx
+++ src/components/WorksPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface IWorksPage {
   title: string
@@ -8,13 +8,17 @@ interface IWorksPage {
 import { titleText, container, item, linkText, text } from './index.module.css'
 
 const WorksPage = ({ title, areas }: IWorksPage) => {
+  const [pathName, setPathName] = useState<string>();
+
+  useEffect(()=>{
+    setPathName(window.location.pathname)
+  }, [])
+
   return (
     <>
       <h2 className={titleText}>{title}</h2>
       <div className={container}>
         {areas.map(({ areaName, link }, i) => {
-          console.log(window.location.pathname);
-          const pathName = window.location.pathname;
           let url = ""
           if( pathName === "/works"){
             url = "works-area"
